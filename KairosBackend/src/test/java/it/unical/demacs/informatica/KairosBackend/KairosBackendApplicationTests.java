1ddafe7325861ad8ed7f2f5b1385c49e1eb9832a
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-04-16 12:29:00 +0200

    Inital commit

---
+++ KairosBackend/src/test/java/it/unical/demacs/informatica/KairosBackend/KairosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.informatica.KairosBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

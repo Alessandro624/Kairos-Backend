bc0437cca485a72425167943f00ed05d64230431
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-04-17 23:10:36 +0200

    Added User entity

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/entities/User.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/entities/User.java
@@ -1,4 +1,93 @@
 package it.unical.demacs.informatica.KairosBackend.data.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+// TODO add interface UserDetails for Spring Security
+// TODO min and max size for strings
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @UuidGenerator(style = UuidGenerator.Style.AUTO)
+    @NotNull(message = "Id cannot be null")
+    @Size(min = 36, max = 36)
+    @Column(name = "id", updatable = false, unique = true, columnDefinition = "UUID")
+    private UUID id;
+
+    @NotBlank(message = "First name cannot be blank")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email cannot be blank")
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    // TODO password validation with custom annotation
+    @NotBlank(message = "Password cannot be blank")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // TODO phone number validation
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @NotNull
+    @Column(name = "email_verified", nullable = false)
+    private boolean emailVerified;
+
+    // TODO role mapping
+    // @Enumerated(EnumType.STRING)
+    // @NotNull(message = "Role cannot be null")
+    // @Column(name = "role", nullable = false)
+    // private UserRole role;
+
+    // TODO authentication provider mapping
+    // @NotNull(message = "Authentication provider cannot be null")
+    // @Enumerated(EnumType.STRING)
+    // @Column(name = "provider", nullable = false)
+    // private Provider provider;
+
+    // TODO mapping with profile image
+    // @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private UserImage profileImage;
+
+    // TODO wishlist mapping
+    // @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<Wishlist> wishlists;
+
+    // TODO tickets mapping (only role == PARTICIPANT)
+    // @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<Ticket> tickets;
+
+    // TODO events mapping (only role == ORGANIZER)
+    // @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<Event> events;
+
+    // TODO implement UserDetails methods
 }

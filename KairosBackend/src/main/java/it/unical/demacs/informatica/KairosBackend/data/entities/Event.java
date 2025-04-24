a6bb2126f08f7ca4bca580a96ef9d25e97d07e7e
Gabriele Grillo <gabriele.grillo.2003@gmail.com>
2025-04-24 15:28:51 +0200

    added column annotations, defined event image

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/entities/Event.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/entities/Event.java
@@ -1,57 +1,54 @@
 package it.unical.demacs.informatica.KairosBackend.data.entities;
 
+import it.unical.demacs.informatica.KairosBackend.data.EventImage;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 
-// gab
-// Evento(ID,titolo,descrizione,data_ora,max_partecipanti,organizzatore_id*,struttura_id*)
-
 @Data
-@Entity
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "event")
 public class Event {
     @Id
-    @GeneratedValue
-    @UuidGenerator
-    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @UuidGenerator(style = UuidGenerator.Style.AUTO)
+    @Column(name = "id", length = 36, nullable = false, updatable = false, unique = true, columnDefinition = "UUID")
     private UUID id;
 
-    @Size(min = 1, max = 50)
+    @Size(min = 1, max = 100)
     @NonNull
-
+    @Column(name = "title", nullable = false)
     private String title;
 
     @Size(min = 1, max = 1000)
     @NonNull
+    @Column(name = "description", nullable = false)
     private String description;
 
     @NonNull
-    private LocalDateTime time;
+    @Column(name = "datetime", nullable = false)
+    private LocalDateTime dateTime;
 
     @Size(min=1)
+    @Column(name = "title", nullable = false)
     private int maxParticipants;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ID_ORGANIZER")
+    @JoinColumn(name = "id_organizer")
     private User organizer;
 
-
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ID_STRUCTURE")
+    @JoinColumn(name = "id_structure")
     private Structure structure;
 
-    // da capire
-    // private List<User> participants = new ArrayList<>();
-    //
-
-
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<EventImage> images;
 }

41a8c7af1807e0852e414cf70b293128d50e8500
garra <garrafa435@gmail.com>
2025-04-24 17:44:17 +0200

    Extended WishlistRepository with JpaSpecificationExecutor

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/repository/WishlistRepository.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/repository/WishlistRepository.java
@@ -3,6 +3,7 @@ package it.unical.demacs.informatica.KairosBackend.data.repository;
 import it.unical.demacs.informatica.KairosBackend.data.entities.Wishlist;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface WishlistRepository extends ListCrudRepository<Wishlist, UUID>, PagingAndSortingRepository<Wishlist, UUID> {
+public interface WishlistRepository extends ListCrudRepository<Wishlist, UUID>, PagingAndSortingRepository<Wishlist, UUID>, JpaSpecificationExecutor<Wishlist> {
 
     Integer countByCreator_Id(UUID creatorId);
 

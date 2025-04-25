d4dff31371aa1a62fc80e72f716e6a67eefdc506
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-04-25 13:17:35 +0200

    Added Phone number validation with custom annotation

---
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/annotations/PhoneNumberValidator.java
@@ -0,0 +1,44 @@
+package it.unical.demacs.informatica.KairosBackend.dto.annotations;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, String> {
+    private int min;
+    private int max;
+    private boolean mustStartWithPlus;
+    private String regex;
+
+    @Override
+    public void initialize(PhoneNumber constraintAnnotation) {
+        this.min = constraintAnnotation.min();
+        this.max = constraintAnnotation.max();
+        this.mustStartWithPlus = constraintAnnotation.mustStartWithPlus();
+        this.regex = constraintAnnotation.regex();
+    }
+
+    @Override
+    public boolean isValid(String phone, ConstraintValidatorContext context) {
+        if (phone.length() < min || phone.length() > max) {
+            setMessage(context, "Phone number must be between " + min + " and " + max + " characters");
+            return false;
+        }
+
+        if (mustStartWithPlus && !phone.startsWith("+")) {
+            setMessage(context, "Phone number must start with '+'");
+            return false;
+        }
+
+        if (!phone.matches(regex)) {
+            setMessage(context, "Phone number contains invalid characters");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void setMessage(ConstraintValidatorContext context, String message) {
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+    }
+}

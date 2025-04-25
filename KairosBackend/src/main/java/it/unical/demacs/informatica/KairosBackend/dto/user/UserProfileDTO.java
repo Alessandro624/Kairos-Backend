e6713759078e4dc6280feacda84e1896c2cd839c
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-04-25 17:06:36 +0200

    Update UserImageService

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/user/UserProfileDTO.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/user/UserProfileDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// TODO can be used for review, just image and username of the User
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

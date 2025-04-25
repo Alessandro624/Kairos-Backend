964d4777f71cca6b5827bee1e09063e2d75cd0a7
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-04-25 13:25:36 +0200

    Added UserProfileDTO

---
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/user/UserProfileDTO.java
@@ -0,0 +1,16 @@
+package it.unical.demacs.informatica.KairosBackend.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserProfileDTO {
+    private String username;
+
+    private UserImageDTO profileImage;
+}

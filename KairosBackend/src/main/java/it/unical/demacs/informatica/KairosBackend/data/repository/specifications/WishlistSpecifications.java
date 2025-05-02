cf7ffdaddf8d2c430d3eb80a2509412277e0543f
garra <garrafa435@gmail.com>
2025-05-02 17:43:54 +0200

    Corrected WishlistSpecifications class due to some refactoring

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/repository/specifications/WishlistSpecifications.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/data/repository/specifications/WishlistSpecifications.java
@@ -2,6 +2,7 @@ package it.unical.demacs.informatica.KairosBackend.data.repository.specification
 
 import it.unical.demacs.informatica.KairosBackend.data.entities.User;
 import it.unical.demacs.informatica.KairosBackend.data.entities.Wishlist;
+import it.unical.demacs.informatica.KairosBackend.dto.wishlist.WishlistFilterDTO;
 import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -11,25 +12,25 @@ import java.util.List;
 public class WishlistSpecifications {
     private WishlistSpecifications() {}
 
-    static Specification<Wishlist> filterWishlists(WishlistFilter wishlistFilter) {
+    public static Specification<Wishlist> filterWishlists(WishlistFilterDTO wishlistFilterDTO) {
         return (root, cq, cb) ->{
 
             List<Predicate> predicates = new ArrayList<>();
 
             //Filter by wishlist name
-            if(wishlistFilter.name != null)
-                predicates.add(cb.like(root.get("name"), "%" + wishlistFilter.name + "%"));
+            if(wishlistFilterDTO.getName() != null)
+                predicates.add(cb.like(root.get("name"), "%" + wishlistFilterDTO.getName() + "%"));
 
             //filter by wishlist scope
-            if(wishlistFilter.wishlistPropertyFilter != null){
+            if(wishlistFilterDTO.getWishlistPropertyFilter() != null){
                 //join necessaria in quanto necessito dell'Id del creatore (e non di User, presente in Wishlist!)
                 Join<Wishlist, User> creatorJoin = root.join("creator", JoinType.LEFT);
                 Join<Wishlist, User> sharedUsersJoin = root.join("sharedUsers", JoinType.LEFT);
 
-                Predicate owned = cb.equal(creatorJoin.get("id"), wishlistFilter.userId);
-                Predicate shared = cb.equal(sharedUsersJoin.get("id"), wishlistFilter.userId);
+                Predicate owned = cb.equal(creatorJoin.get("id"), wishlistFilterDTO.getUserId());
+                Predicate shared = cb.equal(sharedUsersJoin.get("id"), wishlistFilterDTO.getUserId());
 
-                switch (wishlistFilter.wishlistPropertyFilter) {
+                switch (wishlistFilterDTO.getWishlistPropertyFilter()) {
                     case OWNED -> predicates.add(owned);
                     case SHARED -> predicates.add(shared);
                     case BOTH -> {
@@ -41,7 +42,7 @@ public class WishlistSpecifications {
 
             //pass at least a parameter
             if (predicates.isEmpty())
-                predicates.add(cb.equal(root.get("id"), -1L)); // passami almeno un filtro
+                predicates.add(cb.equal(root.get("id"), -1L));
 
 
             return cq.where(cb.or(predicates.toArray(new Predicate[0])))
--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/wishlist/WishlistFilterDTO.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/wishlist/WishlistFilterDTO.java
@@ -1,9 +1,11 @@
 package it.unical.demacs.informatica.KairosBackend.dto.wishlist;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 import java.util.UUID;
 
+@Data
 public class WishlistFilterDTO {
     @NotNull(message = "User id cannot be null")
     UUID userId;

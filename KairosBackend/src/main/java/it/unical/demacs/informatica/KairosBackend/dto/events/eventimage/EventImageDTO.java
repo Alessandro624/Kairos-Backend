5b14e4e8980d1f20bf6cb123277720f9a401d496
Gabriele Grillo <gabriele.grillo.2003@gmail.com>
2025-05-02 12:50:01 +0200

    started working on DTO and added preference index for photos.

---
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/events/eventimage/EventImageDTO.java
@@ -0,0 +1,14 @@
+package it.unical.demacs.informatica.KairosBackend.dto.events.eventimage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EventImageDTO {
+
+}

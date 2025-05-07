8c8be34222e96e4ca964343ffdfe999ae5e8a54c
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-05-08 00:01:46 +0200

    Update Auditor with current auditor logic from security context

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/config/auditor/UserAuditorAware.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/config/auditor/UserAuditorAware.java
@@ -1,16 +1,23 @@
 package it.unical.demacs.informatica.KairosBackend.config.auditor;
 
+import lombok.NonNull;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
-import java.util.UUID;
 
-public class UserAuditorAware implements AuditorAware<UUID> {
+public class UserAuditorAware implements AuditorAware<String> {
+    // TODO check if this is the best way to do it or if we have to store the id in the security context instead of the email/username
 
-    private static final UUID AUTH_CODE = UUID.randomUUID();
     @Override
-    public Optional<UUID> getCurrentAuditor() {
-        //TODO get logged user (Spring security)
-        return Optional.of(AUTH_CODE);
+    @NonNull
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated())
+            return Optional.empty();
+
+        return Optional.of(authentication.getName());
     }
 }

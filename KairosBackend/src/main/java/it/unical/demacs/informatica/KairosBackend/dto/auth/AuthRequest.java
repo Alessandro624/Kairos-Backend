e2e3a72bfc7197edbe8c3f59841790d26a0a9e07
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-05-08 22:19:32 +0200

    Added AuthRequest and AuthResponse dto

---
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/dto/auth/AuthRequest.java
@@ -0,0 +1,20 @@
+package it.unical.demacs.informatica.KairosBackend.dto.auth;
+
+import it.unical.demacs.informatica.KairosBackend.dto.annotations.Password;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+    @NotBlank(message = "Username or email cannot be blank")
+    private String usernameOrEmail;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}

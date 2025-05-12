8c2a6983c06d8c3a43c8a2659abc32f552a43a9a
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-05-12 19:11:27 +0200

    Added role handling for oauth2 and in particular for keycloak

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/config/security/CustomOAuth2UserService.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/config/security/CustomOAuth2UserService.java
@@ -14,10 +14,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -39,26 +36,31 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
             if (user.getProvider() != provider) {
                 throw new OAuth2AuthenticationException("User with email " + user.getEmail() + " already exists");
             }
-            
-            return new OAuth2User() {
-                @Override
-                public Map<String, Object> getAttributes() {
-                    return oAuth2User.getAttributes();
-                }
 
-                @Override
-                public Collection<? extends GrantedAuthority> getAuthorities() {
-                    return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
-                }
+            return getOAuth2User(oAuth2User, user);
 
-                @Override
-                public String getName() {
-                    return user.getUsername();
-                }
-            };
         } catch (Exception e) {
             throw new OAuth2AuthenticationException("Failed to load user information");
         }
+    }
+
+    public OAuth2User getOAuth2User(OAuth2User oAuth2User, User user) {
+        return new OAuth2User() {
+            @Override
+            public Map<String, Object> getAttributes() {
+                return oAuth2User.getAttributes();
+            }
+
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+            }
+
+            @Override
+            public String getName() {
+                return user.getUsername();
+            }
+        };
     }
 
     public User getUserByOAuth2User(OAuth2User oAuth2User, Provider provider) {
@@ -73,6 +75,10 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         // String pictureUrl = (String) oAuth2User.getAttributes().get("picture");
         boolean emailVerified = (boolean) oAuth2User.getAttributes().get("email_verified");
 
+        Collection<GrantedAuthority> authorities = extractRoles(oAuth2User.getAttributes());
+
+        UserRole role = authorities.isEmpty() ? UserRole.USER : UserRole.valueOf(authorities.iterator().next().getAuthority().replace("ROLE_", "").toUpperCase());
+
         return userRepository.findByEmail(email)
                 .orElseGet(() -> {
                     User newUser = new User();
@@ -81,10 +87,56 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                     newUser.setPassword(UUID.randomUUID().toString());
                     newUser.setFirstName(firstName);
                     newUser.setLastName(lastName);
-                    newUser.setRole(UserRole.USER);
+                    newUser.setRole(role);
                     newUser.setProvider(provider);
                     newUser.setEmailVerified(emailVerified);
                     return userRepository.save(newUser);
                 });
+    }
+
+    private Collection<GrantedAuthority> extractRoles(Map<String, Object> claims) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        extractRolesFromRealmAccess(claims, authorities);
+        extractRolesFromResourceAccess(claims, authorities);
+        return authorities;
+    }
+
+    private void extractRolesFromRealmAccess(Map<String, Object> claims, Set<GrantedAuthority> authorities) {
+        Map<String, Object> realmAccess = getMap(claims, "realm_access");
+        if (realmAccess != null) {
+            List<String> roles = getList(realmAccess);
+            addRoles(roles, authorities);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void extractRolesFromResourceAccess(Map<String, Object> claims, Set<GrantedAuthority> authorities) {
+        Map<String, Object> resourceAccess = getMap(claims, "resource_access");
+        if (resourceAccess != null) {
+            resourceAccess.values().forEach(clientAccess -> {
+                if (clientAccess instanceof Map) {
+                    List<String> roles = getList((Map<String, Object>) clientAccess);
+                    addRoles(roles, authorities);
+                }
+            });
+        }
+    }
+
+    private void addRoles(List<String> roles, Set<GrantedAuthority> authorities) {
+        if (roles != null) {
+            roles.forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, Object> getMap(Map<String, Object> claims, String key) {
+        Object value = claims.get(key);
+        return value instanceof Map ? (Map<String, Object>) value : null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<String> getList(Map<String, Object> claims) {
+        Object value = claims.get("roles");
+        return value instanceof List ? (List<String>) value : null;
     }
 }

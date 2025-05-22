b24bc5190a6c011735f16e0478d0b56ddc9134b5
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-05-22 20:09:35 +0200

    Added Open API config for swagger

---
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.informatica.KairosBackend.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecuritySchemes({
+        @SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+})
+public class OpenApiConfig {
+}

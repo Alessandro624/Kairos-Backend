823a1dd0a89230508df997df035d2f9f427c8f62
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-06-12 17:53:28 +0200

    Added redirect URI for frontend callback

--- KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/config/handler/OAuth2AuthenticationHandler.java
+++ KairosBackend/src/main/java/it/unical/demacs/informatica/KairosBackend/config/handler/OAuth2AuthenticationHandler.java
@@ -1,15 +1,14 @@
 package it.unical.demacs.informatica.KairosBackend.config.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import it.unical.demacs.informatica.KairosBackend.core.service.CustomOAuth2UserService;
 import it.unical.demacs.informatica.KairosBackend.core.service.JwtService;
 import it.unical.demacs.informatica.KairosBackend.data.entities.User;
 import it.unical.demacs.informatica.KairosBackend.data.entities.enumerated.Provider;
-import it.unical.demacs.informatica.KairosBackend.dto.auth.AuthResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,8 +16,11 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -28,6 +30,9 @@ public class OAuth2AuthenticationHandler extends SavedRequestAwareAuthentication
     private final UserDetailsService userDetailsService;
     private final CustomOAuth2UserService customOAuth2UserService;
 
+    @Value("${app.oauth2.authorizedRedirectUris}")
+    private String authorizedRedirectUris;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
@@ -35,6 +40,25 @@ public class OAuth2AuthenticationHandler extends SavedRequestAwareAuthentication
 
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        String requestedRedirectUri = (String) request.getSession().getAttribute("redirect_uri_before_oauth");
+        if (requestedRedirectUri == null) {
+            // if not in session try URL
+            requestedRedirectUri = request.getParameter("redirect_uri");
+        }
+
+        if (requestedRedirectUri == null || requestedRedirectUri.isEmpty()) {
+            log.error("Redirect URI non found");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Redirect URI is mandatory");
+            return;
+        }
+
+        List<String> authorizedUris = Arrays.asList(authorizedRedirectUris.split(","));
+        if (!authorizedUris.contains(requestedRedirectUri)) {
+            log.error("Redirect URI not authorized: {}", requestedRedirectUri);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Redirect URI not authorized");
+            return;
+        }
 
         String registrationId = oauthToken.getAuthorizedClientRegistrationId();
         Provider provider = Provider.valueOf(registrationId.toUpperCase());
@@ -47,11 +71,12 @@ public class OAuth2AuthenticationHandler extends SavedRequestAwareAuthentication
         String refreshToken = jwtService.generateRefreshToken(userDetails);
         log.debug("Generated JWT token for user: {}", userDetails.getUsername());
 
-        AuthResponse authResponse = new AuthResponse(token, refreshToken);
-        response.setContentType("application/json");
-        response.setStatus(HttpServletResponse.SC_OK);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(response.getWriter(), authResponse);
-        log.debug("Authentication Response sent to user: {}", userDetails.getUsername());
+        String targetUrl = UriComponentsBuilder.fromUriString(requestedRedirectUri)
+                .queryParam("token", token)
+                .queryParam("refreshToken", refreshToken)
+                .build().toUriString();
+
+        log.debug("Redirecting to client with URL: {}", targetUrl);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
